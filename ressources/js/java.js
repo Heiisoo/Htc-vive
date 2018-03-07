030c8582beb45d568259763eaec3a7c51381d60c
Unknown <damienvignes@hotmail.com>
2018-03-07 16:59:43 +0100

    Fuck

--- ressources/js/java.js
+++ ressources/js/java.js
@@ -1,39 +1,10 @@
 
-/*jslint browser: true*/
-/*global $, jQuery ,AOS*/
-
-
 (function ($) {
 
     "use strict";
 
-    var $window = $(window),
-        $body = $('body'),
+    var $body = $('body'),
         $htcMenu = $('.htc-menu'),
-        $testiSlider = $('.avi-slider'),
-        $screenShotsSlider = $('.screenshots-slider'),
-        $countUp = $('.count-num span'),
-        $teamSlider = $('.team-slider'),
-        $blogSlider = $('.recent-blog-slider');
-
-
-    /*START PRELOADER JS & REFRESH AOS*/
-    $window.on('load', function () {
-        $('.preloader').delay(350).fadeOut('slow');
-        AOS.refresh();
-    });
-    /*END PRELOADER JS & REFRESH AOS*/
-
-    $(document).ready(function () {
-
-
-        /*START AOS JS*/
-        AOS.init({
-            disable: 'mobile',
-            once: true,
-            duration: 600
-        });
-        /*END AOS JS*/
 
         /*START SCROLL SPY JS*/
         $body.scrollspy({
@@ -59,19 +30,5 @@
                 $htcMenu.removeClass("sticky");
             }
         });
-
-
-      });
-
-/*$(window).on('resize', function(event){
-    var windowSize = $(window).width();
-    if(windowSize < 768){
-        $('.dropdown-submenu a').on("click", function(e){
-            $(this).next('ul').toggle();
-            e.stopPropagation();
-            e.preventDefault();
-        });
-    }
-});*/
-
+        /*END MENU JS*/
 }(jQuery));
